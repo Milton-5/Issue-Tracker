d9bbc22bb1ae386da29e2f744b82b4a3c52592f9
Milton-5 <miltonneogi5@gmail.com>
2026-08-10 02:27:34 +0600

    update java.js

--- script/java.js
+++ script/java.js
@@ -1,1 +1,54 @@
-console.log("Hello, World!");
+const loadData = () => {
+   fetch("https://phi-lab-server.vercel.app/api/v1/lab/issues") 
+   .then( (res) => res.json()) // promise of json data
+   .then((json) => displayData(json.data)); // log the json data
+}
+
+const displayData = (issues) => {
+
+    const issueContainer = document.getElementById("issue-container");
+
+    issueContainer.innerHTML = "";
+
+    issues.forEach((issue) => {
+
+        const card = document.createElement("div");
+
+        card.innerHTML = `
+        <div class="bg-white rounded-xl border border-gray-200 shadow-sm overflow-hidden flex flex-col justify-between hover:shadow-md transition-shadow">
+                <div class="h-1 bg-emerald-500"></div>
+                <div class="p-4 flex-1 flex flex-col justify-between">
+                    <div>
+                        <div class="flex justify-between items-center mb-3">
+                            <div class="w-7 h-7 rounded-full bg-emerald-100 flex items-center justify-center text-emerald-600 text-xs">
+                                <img src="assets/Open-Status.png" alt="" class="fa-regular fa-circle-dot"></img>
+                            </div>
+                            <span class="px-2.5 py-0.5 rounded-full text-[10px] font-bold bg-red-100 text-red-500 uppercase tracking-wide">High</span>
+                        </div>
+                        <h3 class="font-semibold text-gray-800 text-sm leading-snug mb-2">${issue.title}</h3>
+                        <p class="text-xs text-gray-500 line-clamp-2 mb-4">${issue.description}</p>
+                    </div>
+
+                    <div class="flex flex-wrap gap-1.5 mb-4">
+                       <span class="inline-flex items-center gap-1 px-2 py-0.5 rounded-full text-[10px] font-semibold bg-red-50 text-red-500 border border-red-200">
+      
+                           <img src="assets/Screenshot 2026-08-03 130035.png" alt="Bug" class="w-3 h-3 object-contain">
+                            BUG
+                       </span>
+                       <span class="inline-flex items-center gap-1 px-2 py-0.5 rounded-full text-[10px] font-semibold bg-amber-50 text-amber-600 border border-amber-200">
+                           <img src="assets/Screenshot 2026-08-03 130048.png" alt="Help" class="w-3 h-3 object-contain">
+                            HELP WANTED
+                        </span>
+                    </div>
+                </div>
+
+                <div class="border-t border-gray-100 px-4 py-2.5 bg-gray-50/50 text-[11px] text-gray-400 flex justify-between items-center">
+                    <span>#1 by john_doe</span>
+                    <span>1/15/2024</span>
+                </div>
+            </div>
+        `;
+
+        issueContainer.append(card);
+    });
+};
